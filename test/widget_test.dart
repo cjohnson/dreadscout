5f1bd2251e45948df1592bb8e8e2ffc5f3b5de00
JohnCollin <collin.g.johnson@gmail.com>
2020-07-13 12:56:26 -0400

    Reorganize Project from flutter create

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dreadscout/main.dart';
+import 'package:dreadscout/dreadscout.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
