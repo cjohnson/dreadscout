4f3fbe940a7aa9c13eb1583432d93c7df341b92a
JohnCollin <collin.g.johnson@gmail.com>
2020-07-13 13:05:13 -0400

    Add CheckboxFormElement Widget Boilerplate

---
+++ lib/form/element/checkbox_form_element.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CheckboxFormElement extends StatefulWidget {
+  const CheckboxFormElement({Key key}) : super(key: key);
+
+  @override
+  _CheckboxFormElementState createState() => _CheckboxFormElementState();
+}
+
+class _CheckboxFormElementState extends State<CheckboxFormElement> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
