b8619a6f298329d8e80e757ea65efc718e8c0bee
cjohnson <collin.g.johnson@gmail.com>
2021-08-24 17:22:38 -0400

    Finished styling appbar portion

--- lib/main.dart
+++ lib/main.dart
@@ -53,70 +53,78 @@ class FormPage extends StatelessWidget {
           flexibleSpace: FlexibleSpaceBar(
             collapseMode: CollapseMode.pin,
             centerTitle: true,
-            background: Stack(
-              children: [
-                Positioned.fill(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(30.0),
-                    child: ColorFiltered(
-                      colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.colorBurn),
-                      child: Image(
-                        image: AssetImage('asset/image/dreadbots.jpg'),
-                        fit: BoxFit.fitHeight,
+            background: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(bottom: Radius.circular(30.0)),
+                boxShadow: [
+                  BoxShadow(color: Colors.black26, offset: Offset(0.0, 4.0), blurRadius: 2.0)
+                ]
+              ),
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30.0),
+                      child: ColorFiltered(
+                        colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.colorBurn),
+                        child: Image(
+                          image: AssetImage('asset/image/dreadbots.jpg'),
+                          fit: BoxFit.fitHeight,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Positioned(
-                  left: 28.0,
-                  bottom: 28.0,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Text(
-                        '3656',
-                        style: GoogleFonts.roboto(
-                            textStyle: Theme.of(context).textTheme.headline1,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w800,
-                            fontSize: 44.0,
-                            height: 0.5
-                        ),
-                      ),
-                      Text(
-                        'Dreadbots',
-                        style: GoogleFonts.roboto(
-                            textStyle: Theme.of(context).textTheme.headline1,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w700,
-                            fontSize: 32.0
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            FontAwesomeIcons.mapMarkerAlt,
-                            size: 20.0,
-                            color: Colors.white,
-                          ),
-                          SizedBox(width: 5.0,),
-                          Text(
-                            'From Dexter, Michigan, USA',
-                            style: GoogleFonts.nunito(
-                              textStyle: Theme.of(context).textTheme.subtitle1,
+                  Positioned(
+                    left: 28.0,
+                    bottom: 28.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Text(
+                          '3656',
+                          style: GoogleFonts.roboto(
+                              textStyle: Theme.of(context).textTheme.headline1,
                               color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0
+                              fontWeight: FontWeight.w800,
+                              fontSize: 44.0,
+                              height: 0.5
+                          ),
+                        ),
+                        Text(
+                          'Dreadbots',
+                          style: GoogleFonts.roboto(
+                              textStyle: Theme.of(context).textTheme.headline1,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 32.0
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              FontAwesomeIcons.mapMarkerAlt,
+                              size: 20.0,
+                              color: Colors.white,
                             ),
-                          )
-                        ],
-                      )
-                    ],
+                            SizedBox(width: 5.0,),
+                            Text(
+                              'From Dexter, Michigan, USA',
+                              style: GoogleFonts.nunito(
+                                textStyle: Theme.of(context).textTheme.subtitle1,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20.0
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    )
                   )
-                )
-              ],
+                ],
+              ),
             ),
           ),
         )
